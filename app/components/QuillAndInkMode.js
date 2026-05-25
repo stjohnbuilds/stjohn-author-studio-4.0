2e7c270ec1e2f1355d9c72119363c9a26870b88e
stjohnbuilds <209044074+stjohnbuilds@users.noreply.github.com>
2026-05-25 02:55:18 -0700

    auto-backup: before edit 2026-05-25 02:55:18

--- app/components/QuillAndInkMode.js
+++ app/components/QuillAndInkMode.js
@@ -19,6 +19,7 @@
 import { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import ImportFlow from './ImportFlow';
 import BookDetail, { ChapterRow } from './BookDetail';
+import ProofBookDetail from './SessionsView';
 import ChapterReader, {
   getChapterReaderWordEl,
   computeChapterReaderPopoverPos,

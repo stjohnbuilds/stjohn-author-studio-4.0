0178445d7d8a6cbc699aef4d3680d098444d200f
stjohnbuilds <209044074+stjohnbuilds@users.noreply.github.com>
2026-06-07 18:43:54 -0700

    auto-backup: before edit 2026-06-07 18:43:54

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import PrebuildMode from './components/PrebuildMode';
 import PrepManuscriptMode from './components/PrepManuscriptMode';
 import QuillAndInkMode from './components/QuillAndInkMode';
 import LoginScreen from './components/LoginScreen';
-import { modeAccentVars, ProfilePill } from './components/ReaderChrome';
+import { modeAccentVars, ProfilePill, AppFooter } from './components/ReaderChrome';
 import { countWordsInHtml, normalizeBookPaging } from './lib/manuscriptPaging';
 import { buildSlimPageMap } from './lib/pdfPaging';
 import {
